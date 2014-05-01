c8601134f31336cbdb08f367d18d7820eabe5b90
avigado <avigado@outlook.com>
2014-05-02 01:58:34 +0300

    removing log depedency

--- start.js
+++ start.js
@@ -1,9 +1,6 @@
 // web.js
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
-
-app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
